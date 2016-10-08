44b000715879e32c6a726a63dc2e1dfd2d74f5eb
Santos <santos@elumen.info>
2016-10-08 16:30:01 -0300

    SPLINES-04 - Modularization and new cps method implemented.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,11 +1,15 @@
 
 #include "main.hpp"
 
+
+
+std::vector<double> smCpsRm(MatrixXd mta, MatrixXd mtb);
+
 int main() {
 
     double alfa = 1;
     double beta = 1;
-    int sample = 20;
+    int sample = 9;
 
 
     cv::Mat image= cv::imread("C:\\Users\\Santos\\Desktop\\spoon-4.jpg",0);
@@ -16,34 +20,45 @@ int main() {
 
     //Find the contours. Use the contourOutput Mat so the original image doesn't get overwritten
     std::vector<std::vector<cv::Point> > contours;
+    std::vector<cv::Vec4i > hir;
     cv::Mat contourOutput = image.clone();
     cv::Mat mathEdge;
     cv::threshold(contourOutput, mathEdge, 192.0, 255.0, CV_THRESH_BINARY_INV);
-    cv::findContours( contourOutput, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE );
+    cv::findContours( contourOutput, contours, hir, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE );
+    int pointsNumber = 0;
+
+    int countContourPoints = 0;
+
     int points = sample;
 
     double X[points];
     double Y[points];
 
-    /*Extract points into arrays*/
-    convertPoints(X, Y, contours, sample);
+    std::vector<cv::Point> contour = extractContourPoints(contours, sample);
 
-    cvx::CpsMatrix mta = generateCpsWithSplineRefinement(points, X, Y, contours);
-    cvx::CpsMatrix mtb = generateCpsWithSplineRefinement(points, X, Y, contours);
+    MatrixXd mta = generateCpsWithSplineRefinement(points, contour, contours);
 
-    /* TODO: struct of this.
-     * the X(METRIC 1) coordenate is the minim sum and the Y cordenate is the index of that column on the matrix cpsA*/
-    cv::Point2d matchingData = matchingCps( mta, mtb);
-    /* Result distance of point matching*/
-    double dist1 = matchingData.x;
 
-    /*Metric 2 implementation in progress*/
-
-    /* Result distance of afin transformation*/
-    double dist2 = 1;
-
-    double finalDisimilarityDistance = dist1*alfa + beta*dist2;
+  /*  std::vector<double> dist = smCpsRm( mta, mtb);*/
 }
+
+
+
+/**
+ * This method get the distance between two cps matrix.
+ *//*
+std::vector<double> smCpsRm(MatrixXd mta, MatrixXd mtb) {
+    std::vector<double> dist;
+    int f = mta.rows();
+    for(int i = 0; i < f; i++){
+
+        for(int k = 0; k < f; k++){
+
+        }
+
+    }
+    return dist;
+}*/
 
 
 
