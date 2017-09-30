6bd21f99ab4480e5581a9667f3080c3bedc0cdaa
g-ramos <ragen93@gmail.com>
2017-09-30 11:05:07 -0400

    Changes to command line argument management

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,11 +3,16 @@
 
 int main(int argc, char *argv[]){
 
+    std::cout << "Initializing Experiment:\n";
     //define experiment parameters here
-    std::string imageDbBasePath = "D:\\FP-UNA\\TesisFinalExperiments\\ImageDatabases\\MPEG7_SeparationExperiment\\";
-    std::string resultsBasePath = "D:\\FP-UNA\\TesisFinalExperiments\\ExperimentResults\\";
-    int sampleSize = atoi(argv[1]);
-    double noiseLevel = 25.0;
+    std::string imageDbBasePath(argv[1]);
+    std::cout << " - Image Database Path: " << imageDbBasePath << "\n";
+    std::string resultsBasePath(argv[2]);
+    std::cout << " - Results Folder: " << resultsBasePath << "\n";
+    int sampleSize = atoi(argv[3]);
+    std::cout << " - Sample Size: " << sampleSize << "\n";
+    double noiseLevel = atof(argv[4]);
+    std::cout << " - Noise (dB): " << ((noiseLevel < 0.0) ? "Not using noise." : argv[4]) << "\n\n";
 
     Coefficients = initializeEquationSolver(sampleSize);
 
